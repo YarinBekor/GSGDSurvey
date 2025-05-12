f481c8a11a0af6c80946bee11b1b1b8145402a03
Yarin Bekor <yarin.bekor@gmail.com>
2025-05-12 12:41:43 +0300

    loops working

--- infloop.py
+++ infloop.py
@@ -1,26 +1,13 @@
-from PIL import Image
-import sys
+from PIL import Image, ImageSequence
 
-def make_gif_loop_infinite(input_path, output_path):
-    with Image.open(input_path) as img:
-        frames = []
-        try:
-            while True:
-                frames.append(img.copy())
-                img.seek(len(frames))  # Go to next frame
-        except EOFError:
-            pass  # End of frames
+input_path = "Variants/DG4D/1/step_22.5.gif"
+output_path = "Variants/DG4D/1/step_22.5.gif"
 
-        frames[0].save(
-            output_path,
-            save_all=True,
-            append_images=frames[1:],
-            loop=0,
-            duration=img.info.get('duration', 100),
-            disposal=2
-        )
-        print(f"Saved infinite-looping GIF to: {output_path}")
-
-# Usage example
-if __name__ == "__main__":
-    make_gif_loop_infinite("Variants/DG4D/1/step_22.5.gif", "Variants/DG4D/1/step_22.5.gif")
+with Image.open(input_path) as im:
+    frames = [frame.copy() for frame in ImageSequence.Iterator(im)]
+    im.save(
+        output_path,
+        save_all=True,
+        append_images=frames[1:],
+        loop=0
+    )

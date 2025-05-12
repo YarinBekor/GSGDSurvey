47b2cefe51151cf0276d44a2aff1642b3f115d2d
Yarin Bekor <yarin.bekor@gmail.com>
2025-05-12 12:42:17 +0300

    fixed dg4d variant

--- infloop.py
+++ infloop.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageSequence
 
-input_path = "Variants/DG4D/1/step_22.5.gif"
-output_path = "Variants/DG4D/1/step_22.5.gif"
+input_path = "Variants/DG4D/1/step_247.5.gif"
+output_path = "Variants/DG4D/1/step_247.5.gif"
 
 with Image.open(input_path) as im:
     frames = [frame.copy() for frame in ImageSequence.Iterator(im)]
